0900f641b3e6ccfa4ee5729a4595595925dc141b
David Oswald <daoswald@gmail.com>
2014-09-01 00:31:36 -0600

    Added Inline-prereq boilerplate.

--- lib/Acme/Hello/World/XS.pm
+++ lib/Acme/Hello/World/XS.pm
@@ -1,8 +1,23 @@
 package Acme::Hello::World::XS;
+
+use strict;
+use warnings;
+
 our $VERSION = '0.01;';
 
-use Inline C => <<'...';
+require Exporter;
+our @ISA    = qw( Exporter );
+our @EXPORT = qw( greet    );
+
+use Inline
+  VERSION => '0.01',
+  NAME    => 'Acme::Hello::World::XS',
+  C       => <<'...';
 void greet() { printf("Hello, world\n") }
 ...
+
+END {
+  Inline->init();
+}
 
 1;
